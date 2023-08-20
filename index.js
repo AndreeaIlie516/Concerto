5dad6a8f9f52bf53097ad208d1e01f3474e10ae7
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-08-20 22:11:48 +0300

    Index formatted

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoString = process.env.DATABASE_URL
+const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
-const database = mongoose.connection
+const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 const app = express();
 const port = 4000;
 
-
 app.use(express.json());
 
-
-app.listen(port, () => { console.log(`Hello world app listening on port ${port}!`);});
+app.listen(port, () => {
+  console.log(`Hello world app listening on port ${port}!`);
+});
