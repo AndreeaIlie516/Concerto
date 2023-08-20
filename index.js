f7e705eaac4a4de7269b11aaad8a37e08d894fb4
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-08-20 21:41:51 +0300

    Index added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+const port = 4000;
+
+
+app.use(express.json());
+
+
+app.listen(port, () => { console.log(`Hello world app listening on port ${port}!`);});
