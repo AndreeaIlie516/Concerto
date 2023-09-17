011e40f6177b0b2d455bf64be9b0e4cfcdb32236
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-09-17 14:57:56 +0300

    Category removed

--- routes/categoryRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-  const Controller = require("../controllers/categoryController.js");
-  app.get("/category/getAllCategories", Controller.getAllCategories);
-  app.get("/category/getCategoryByID/:ID", Controller.getCategoryByID);
-  app.post("/category/addCategory", Controller.addCategory);
-  app.delete("/category/deleteCategory/:ID", Controller.deleteCategory);
-  app.put("/category/updateCategory/:ID", Controller.updateCategory);
-};
