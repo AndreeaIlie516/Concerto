cf015a0f8cae57672bc67dedc4ef175586f4c488
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-09-17 14:56:37 +0300

    Agency removed

--- routes/agencyRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-  const Controller = require("../controllers/agencyController.js");
-  app.get("/agency/getAllAgencies", Controller.getAllAgencies);
-  app.get("/agency/getAgencyByID/:ID", Controller.getAgencyByID);
-  app.post("/agency/addAgency", Controller.addAgency);
-  app.delete("/agency/deleteAgency/:ID", Controller.deleteAgency);
-  app.put("/agency/updateAgency/:ID", Controller.updateAgency);
-};
